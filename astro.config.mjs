f12fec16d4db816e79becf60e53403f1dcf93a62
whiskey-d <git@stackblitz.com>
2024-11-16 21:21:36 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  site: 'https://smalldogessentials.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static',
+  adapter: netlify(),
+  build: {
+    inlineStylesheets: 'auto'
+  }
+});
